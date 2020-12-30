5409f012c9323a9256e7c2766cf902453c6859e9
Nhan Cao <nhancv92@gmail.com>
2020-12-30 11:57:53 +0700

    feat: update gen model

--- gen_model.dart
+++ gen_model.dart
@@ -4,16 +4,20 @@ import 'dart:io';
 const String json = '''
 {
   "id": 0,
-  "url": "string",
-  "socialable_id": 0,
-  "socialable_type": "string",
-  "social_media_slug": "string",
-  "icon": "string",
   "name": "string",
-  "test": {
-    "id": 0
+  "starts_at": "string",
+  "ends_at": "string",
+  "is_finished": true,
+  "rating": {
+    "slug": "string",
+    "name": "string"
   },
-  "empty": {}
+  "social_media_links": [{
+    "id": 0,
+    "url": "string",
+    "icon": "string",
+    "name": "string"
+  }],
 }
 ''';
 
@@ -30,6 +34,7 @@ class Template {
 
   @override
   String toString() {
+    // ignore: leading_newlines_in_multiline_strings
     return 'Template{}';
   }
 }
@@ -38,10 +43,30 @@ class Template {
 /// Cache model name here
 Map<String, int> modelNames = <String, int>{};
 
-/// Convert to class name
+/// Convert to Uppercase first char
 /// Ex: socialable => Socialable
+String standardFieldName(String propertyName) {
+  final String name =
+      '${propertyName.substring(0, 1).toUpperCase()}${propertyName.substring(1)}';
+  return name;
+}
+
+/// Convert to class name, remove last 's'
+/// Ex: socialables => Socialable
 String standardClassName(String propertyName) {
-  return '${propertyName.substring(0, 1).toUpperCase()}${propertyName.substring(1)}';
+  final String name = standardFieldName(propertyName);
+  if (name.endsWith('s')) {
+    return name.substring(0, name.length - 1);
+  }
+  return name;
+}
+
+/// Standard file name from json key
+String standardClassFile(String jsonKey) {
+  if (jsonKey.endsWith('s')) {
+    jsonKey = jsonKey.substring(0, jsonKey.length - 1);
+  }
+  return '$jsonKey.dart';
 }
 
 /// Convert json key to dart key
@@ -49,7 +74,7 @@ String standardClassName(String propertyName) {
 String standardPropertyName(String jsonKey) {
   List<String> comps = jsonKey.split('_');
   for (int i = 1; i < comps.length; i++) {
-    comps[i] = standardClassName(comps[i]);
+    comps[i] = standardFieldName(comps[i]);
   }
   return comps.join();
 }
@@ -68,6 +93,8 @@ String findValueType(dynamic value) {
     type = 'String';
   } else if (value is List<dynamic>) {
     type = 'List<dynamic>';
+  } else if (value != null) {
+    type = 'object';
   }
   return type;
 }
@@ -93,14 +120,25 @@ Future<void> createModel(
     String type = findValueType(value);
     String propertyName = standardPropertyName(key);
     String className = standardClassName(propertyName);
-    String classFile = '$key.dart';
-    if (type == 'dynamic' && !modelNames.containsKey(className)) {
+    String classFile = standardClassFile(key);
+    if (type == 'object' && !modelNames.containsKey(className)) {
       createModel(classFile, className, value);
 
       /// Add imports
       String importsPattern = 'class $rootModelClassName {';
       String tmpImports = 'import \'$classFile\';\n$importsPattern';
       classContent = classContent.replaceAll(importsPattern, tmpImports);
+    } else if (type == 'List<dynamic>' && value is List && value.length > 0) {
+      dynamic firstI = value[0];
+      String listType = findValueType(firstI);
+      if (listType == 'object' && !modelNames.containsKey(className)) {
+        createModel(classFile, className, firstI);
+
+        /// Add imports
+        String importsPattern = 'class $rootModelClassName {';
+        String tmpImports = 'import \'$classFile\';\n$importsPattern';
+        classContent = classContent.replaceAll(importsPattern, tmpImports);
+      }
     }
   });
 
@@ -128,10 +166,22 @@ Future<void> createModel(
     decodedJson.forEach((String key, dynamic value) {
       String standardName = standardPropertyName(key);
       String type = findValueType(value);
-      if (type == 'dynamic') {
+      if (type == 'object') {
         String className = standardClassName(standardName);
         tmpFactory +=
             '\t\t\t\t$standardName: json[\'$key\'] != null ? $className.fromJson(json[\'$key\'] as Map<String, dynamic>) : null,\n';
+      } else if (type == 'List<dynamic>' && value is List && value.length > 0) {
+        dynamic firstI = value[0];
+        String listType = findValueType(firstI);
+        if (listType == 'object') {
+          String propertyName = standardPropertyName(key);
+          String className = standardClassName(propertyName);
+          tmpFactory +=
+              '\t\t\t\t$standardName: json[\'$key\'] != null ? List<$className>.from((json[\'$key\'] as List<dynamic>).map<$className>((dynamic x) => $className.fromJson(x as Map<String, dynamic>))) : <$className>[],\n';
+        } else {
+          tmpFactory +=
+              '\t\t\t\t$standardName: List<$listType>.from(json[\'$key\'] as List<dynamic>),\n';
+        }
       } else {
         tmpFactory += '\t\t\t\t$standardName: json[\'$key\'] as $type,\n';
       }
@@ -145,9 +195,19 @@ Future<void> createModel(
     decodedJson.forEach((String key, dynamic value) {
       String standardName = standardPropertyName(key);
       String type = findValueType(value);
-      if (type == 'dynamic') {
+      if (type == 'object') {
         String className = standardClassName(standardName);
         tmpProperties += '\tfinal $className $standardName;\n';
+      } else if (type == 'List<dynamic>' && value is List && value.length > 0) {
+        dynamic firstI = value[0];
+        String listType = findValueType(firstI);
+        if (listType == 'object') {
+          String propertyName = standardPropertyName(key);
+          String className = standardClassName(propertyName);
+          tmpProperties += '\tfinal List<$className> $standardName;\n';
+        } else {
+          tmpProperties += '\tfinal List<$type> $standardName;\n';
+        }
       } else {
         tmpProperties += '\tfinal $type $standardName;\n';
       }
@@ -159,7 +219,23 @@ Future<void> createModel(
     String tmpToJson = '<String, dynamic>{\n';
     decodedJson.forEach((String key, dynamic value) {
       String standardName = standardPropertyName(key);
-      tmpToJson += '\t\t\t\t\'$key\': $standardName,\n';
+      String type = findValueType(value);
+      if (type == 'object') {
+        tmpToJson += '\t\t\t\t\'$key\': $standardName?.toJson(),\n';
+      } else if (type == 'List<dynamic>' && value is List && value.length > 0) {
+        dynamic firstI = value[0];
+        String listType = findValueType(firstI);
+        if (listType == 'object') {
+          String propertyName = standardPropertyName(key);
+          String className = standardClassName(propertyName);
+          tmpToJson +=
+              '\t\t\t\t\'$key\': List<$className>.from($standardName.map<Map<String, dynamic>>(($className x) => x.toJson())),\n';
+        } else {
+          tmpToJson += '\t\t\t\t\'$key\': $standardName,\n';
+        }
+      } else {
+        tmpToJson += '\t\t\t\t\'$key\': $standardName,\n';
+      }
     });
     tmpToJson += '\t\t\t};';
     classContent = classContent.replaceFirst(toJsonPattern, tmpToJson);
@@ -179,7 +255,7 @@ Future<void> createModel(
 
 Future<void> main() async {
   final Map<String, dynamic> decodedJson = jsonDecode(json);
-  final String rootModelFileName = 'rating.dart';
-  final String rootModelClassName = 'Rating';
+  final String rootModelFileName = 'event.dart';
+  final String rootModelClassName = 'Event';
   createModel(rootModelFileName, rootModelClassName, decodedJson);
 }
